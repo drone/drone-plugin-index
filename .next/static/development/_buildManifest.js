1355f915d1526536130734fe6434ed9905988874
Dan Wilson <dwilson89@live.co.uk>
2022-03-31 14:41:49 +0100

    Delete .next directory

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/plugins/[id]":["static\u002Fchunks\u002Fpages\u002Fplugins\u002F[id].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fplugins\u002F[id]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
