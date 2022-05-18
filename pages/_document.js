848c790bf3056a492e05e8cda0084d0861738996
Dan Wilson <daniel.wilson@harness.io>
2022-05-18 10:46:01 +0100

    add lang as per lighthouse report

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&family=Roboto+Mono&display=swap"
